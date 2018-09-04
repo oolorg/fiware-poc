50b7ba262af805dd77d950adfcb5790e4d9059bf
YujiAzama <yuji.azama@gmail.com>
2018-09-04 16:00:54 +0900

    Referctor scripts

--- pocs/publish_container/del_containers.sh
+++ pocs/publish_container/del_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for container in `docker ps | grep pub.sh | awk '{print $10}'`;
+for container in `docker ps | grep pub.sh | awk '{print $1}'`;
 do
-    docker rm --force $container
+    docker rm --force $container &
     echo "Remove "$container
 done
