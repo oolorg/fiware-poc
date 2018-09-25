e17a60369f7e8a7f1a4e89a4c3c25047c1af77d6
hiro-noguchi <noguchi0306@gmail.com>
2018-09-26 01:37:57 +0900

    TOPICとメッセージをFIWAREにアップロードできる形に変更

--- pocs/publish_container/pub.sh
+++ pocs/publish_container/pub.sh
@@ -4,7 +4,7 @@ count=0
 
 while :
 do
-    mosquitto_pub -h ${HOST} -u ${USERNAME} -P ${PASSWORD} -t ${TOPIC} -m /apikey/${DEVICE_ID}/seq/${count}
+    mosquitto_pub -h ${HOST} -u ${USERNAME} -P ${PASSWORD} -t ${TOPIC} -m "{\"m\":\"/apikey/${DEVICE_ID}/seq/${count}\"}"
     count=`expr $count + 1`
     sleep ${PUB_INTERVAL}
 done
--- pocs/publish_container/run-containers.sh
+++ pocs/publish_container/run-containers.sh
@@ -4,6 +4,6 @@ id=0
 
 for i in `seq 1 $1`
 do
-    docker run -d -e HOST=172.18.0.1 -e PUB_INTERVAL=5 -e DEVICE_ID=device${id} --name device${id} pub &
+    docker run -d -e HOST=192.168.28.10 -e PUB_INTERVAL=5 -e DEVICE_ID=device${id} -e TOPIC=/apikey/device${id}/attrs --name device${id} pub &
     id=`expr $id + 1`
 done
