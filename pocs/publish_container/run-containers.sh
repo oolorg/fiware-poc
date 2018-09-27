c06404e026e69fe1687b64f0349bccfb914d0d09
hiro-noguchi <noguchi0306@gmail.com>
2018-09-27 05:19:52 +0000

    起動するdockerのネットワークモードの変更。バックグランド処理を削除

--- pocs/publish_container/run-containers.sh
+++ pocs/publish_container/run-containers.sh
@@ -1,9 +1,9 @@
 NUMBER_OF_CONTAINERS=$1
 
-id=0
+id=$2
 
 for i in `seq 1 $1`
 do
-    docker run -d -e HOST=192.168.28.10 -e PUB_INTERVAL=5 -e DEVICE_ID=device${id} -e TOPIC=/apikey/device${id}/attrs --name device${id} pub &
+    docker run -d --net=host -e HOST=192.168.28.10 -e PUB_INTERVAL=5 -e DEVICE_ID=device${id} -e TOPIC=/apikey/device${id}/attrs --name device${id} pub
     id=`expr $id + 1`
 done
