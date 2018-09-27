1b60736682ff24b782502e01ac3d10a3578b6104
hiro-noguchi <noguchi0306@gmail.com>
2018-09-27 14:04:28 +0000

    ログを標準出力するように変更

--- pocs/publish_container/pub.sh
+++ pocs/publish_container/pub.sh
@@ -5,6 +5,7 @@ count=0
 while :
 do
     mosquitto_pub -h ${HOST} -u ${USERNAME} -P ${PASSWORD} -t ${TOPIC} -m "{\"m\":\"/apikey/${DEVICE_ID}/seq/${count}\"}"
+    echo "{\"m\":\"/apikey/${DEVICE_ID}/seq/${count}\"}"
     count=`expr $count + 1`
     sleep ${PUB_INTERVAL}
 done
