7f5f0835289ad227dd8761dc13c19a9a46a950f4
hiro-noguchi <noguchi0306@gmail.com>
2018-09-27 14:06:39 +0000

    intervalを起動時に指定できるように変更。ログをホストのsyslogに残すように変更

--- pocs/publish_container/run-containers.sh
+++ pocs/publish_container/run-containers.sh
@@ -1,9 +1,10 @@
 NUMBER_OF_CONTAINERS=$1
-
-id=$2
+interval=$2
+id=$3
 
 for i in `seq 1 $1`
 do
-    docker run -d --net=host -e HOST=192.168.28.10 -e PUB_INTERVAL=5 -e DEVICE_ID=device${id} -e TOPIC=/apikey/device${id}/attrs --name device${id} pub
+    docker run -d --net=host -e HOST=192.168.28.10 -e PUB_INTERVAL=${interval} -e DEVICE_ID=device${id} -e TOPIC=/apikey/device${id}/attrs --name device${id} --log-driver=syslog pub &
     id=`expr $id + 1`
+    sleep 1
 done
