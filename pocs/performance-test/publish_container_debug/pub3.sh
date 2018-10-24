41488d5f3df743323bb600e0389ab457f064747b
hiro-noguchi <noguchi0306@gmail.com>
2018-10-25 08:18:53 +0900

    擬似デバイスコンテナの改良(ホスト名の追加)

--- pocs/performance-test/publish_container_debug/pub3.sh
+++ pocs/performance-test/publish_container_debug/pub3.sh
@@ -4,11 +4,11 @@ count=0
 
 for i in `seq 1 ${MAX}`
 do
-    mosquitto_pub -h ${HOST} -u ${USERNAME} -P ${PASSWORD} -t ${TOPIC} -i ${DEVICE_ID}_${count} -m "{\"m\":\"/apikey/${DEVICE_ID}/seq/${count}\"}" -d
+    mosquitto_pub -h ${HOST} -u ${USERNAME} -P ${PASSWORD} -t ${TOPIC} -i `hostname`_${DEVICE_ID}_${count} -m "{\"m\":\"/`hostname`/apikey/${DEVICE_ID}/seq/${count}\"}" -d
     if [ $? = 0 ]; then
-       echo "{\"succes\":\"/apikey/${DEVICE_ID}/seq/${count}\"}"
+       echo "{\"succes\":\"/`hostname`/apikey/${DEVICE_ID}/seq/${count}\"}"
     else
-       echo "{\"error\":\"/apikey/${DEVICE_ID}/seq/${count}\"}"
+       echo "{\"error\":\"/`hostname`/apikey/${DEVICE_ID}/seq/${count}\"}"
     fi
     count=`expr $count + 1`
     sleep ${PUB_INTERVAL}
