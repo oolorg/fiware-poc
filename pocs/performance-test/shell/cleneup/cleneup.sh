479bb1e7166d34d2c9fdfbe459d5e721d1cf7536
hiro-noguchi <noguchi0306@gmail.com>
2018-12-12 23:12:36 +0900

    fix performance test

--- pocs/performance-test/shell/cleneup/cleneup.sh
+++ pocs/performance-test/shell/cleneup/cleneup.sh
@@ -7,6 +7,8 @@ sudo kill -9 `ps -ef | grep 01-get_fiware_cpu_docker_stats.sh | grep -v "grep" |
 sudo kill -9 `ps -ef | grep metrics-cygnus.sh | grep -v "grep" | awk '{print $2}'`
 sudo kill -9 `ps -ef | grep metrics-orion.sh | grep -v "grep" | awk '{print $2}'`
 sudo kill -9 `ps -ef | grep localhost:2376/containers/mongodb-comet-demo/stats | grep -v "grep" | awk '{print $2}'`
+sudo kill -9 `ps -ef | grep sar | grep -v "grep" | awk '{print $2}'`
+sudo kill -9 `ps -ef | grep stats-cygnus.sh | grep -v "grep" | awk '{print $2}'`
 
 ${TEST_HOME}/pocs/performance-test/shell/logging/metrics-mosquitto.sh
 ${TEST_HOME}/pocs/performance-test/shell/logging/statistics-orion.sh
@@ -18,6 +20,7 @@ ${TEST_HOME}/pocs/performance-test/shell/cleneup/get_data_from_comet.sh ${NUMBER
 
 docker cp cygnus-demo:/gc.log ${TEST_HOME}/pocs/performance-test/log/cygnus-gc.log
 docker cp cygnus-demo:/heap-dump.hprof ${TEST_HOME}/pocs/performance-test/log/cygnus-heap-dump.hprof
+docker cp cygnus-demo:/hs_err_pid20.log ${TEST_HOME}/pocs/performance-test/log/cygnus-hs_err_pid20.log
 
 
 
