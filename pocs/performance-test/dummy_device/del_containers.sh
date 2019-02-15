940ae3f7286c78108cc1b87ac0efe427f20bb515
hiro-noguchi <noguchi0306@gmail.com>
2019-02-15 02:00:08 +0000

    fix performance-test

--- pocs/performance-test/dummy_device/del_containers.sh
+++ pocs/performance-test/dummy_device/del_containers.sh
@@ -2,6 +2,8 @@
 
 for container in `docker ps -a | grep dummy_device | awk '{print $1}'`;
 do
-    docker rm --force $container &
+    docker rm --force $container &>/dev/null
     echo "Remove "$container
 done
+
+expr `docker ps -a |wc -l` - 1
