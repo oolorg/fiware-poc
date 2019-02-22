31f5b38f7c7437db45ce79a8173dba3f645de3c4
hiro-noguchi <noguchi0306@gmail.com>
2019-02-22 02:36:40 +0000

    fix performance-test

--- pocs/performance-test/shell/before_logging/before_logging.sh
+++ pocs/performance-test/shell/before_logging/before_logging.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ${TEST_HOME}/pocs/performance-test/shell/before_logging/get_mosquitto_metrics.sh
-${TEST_HOME}/pocs/performance-test/shell/before_logging/get_host_metrics.sh 1
 ${TEST_HOME}/pocs/performance-test/shell/before_logging/get_orion_subscription.sh
 ${TEST_HOME}/pocs/performance-test/shell/before_logging/get_veth.sh
+
+${TEST_HOME}/pocs/performance-test/shell/before_logging/get_host_metrics.sh 1
+${TEST_HOME}/pocs/performance-test/shell/before_logging/get_cygnus_metrics.sh
 ${TEST_HOME}/pocs/performance-test/shell/before_logging/get_fiware_docker_stats.sh &
