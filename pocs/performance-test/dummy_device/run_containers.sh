c5f80f2dbed108f98040916b20ca828ed598d42d
hiro-noguchi <noguchi0306@gmail.com>
2019-03-11 02:06:18 +0000

    fix performance-test

--- pocs/performance-test/dummy_device/run_containers.sh
+++ pocs/performance-test/dummy_device/run_containers.sh
@@ -17,7 +17,7 @@ do
     -e VM_ID=vm1 \
     -e DEVICE_ID=device`printf %05g ${i}` \
     -e SEND_INTERVAL=${SEND_INTERVAL} \
-    -e MESSAGE_NUM=$((${TEST_TIME}/${SEND_INTERVAL})) \
+    -e MESSAGE_NUM=$(((${TEST_TIME}+${NUMBER_OF_CONTAINERS}*${STARTUP_TIME})/${SEND_INTERVAL})) \
     -e STARTUP_INTERVAL=$((${STARTUP_TIME}*${i})) \
     -e DATA_TYPE=${DATA_TYPE} \
     --name device`printf %05g ${i}` \
@@ -26,7 +26,7 @@ do
 done
 
 # 32400は9時間の秒換算(UTCをJSTに変換するために使用)
-EXPECTED_FINISH_TIME=$((${NUMBER_OF_CONTAINERS}*${STARTUP_TIME}*2+${MESSAGE_NUM}+32400))
+EXPECTED_FINISH_TIME=$((${NUMBER_OF_CONTAINERS}*${STARTUP_TIME}*2+${TEST_TINE}+32400))
 
 echo "Number of containers"
 echo $((`docker ps |wc -l`-1))
@@ -35,4 +35,4 @@ echo "Expected finish time"
 date --date "${EXPECTED_FINISH_TIME} seconds" "+%m/%d %H:%M:%S"
 echo ""
 echo "Number of all MQTT messages"
-echo $((${TEST_TIME}/${SEND_INTERVAL}*${NUMBER_OF_CONTAINERS}))
+echo $(((${TEST_TIME}+${NUMBER_OF_CONTAINERS}*${STARTUP_TIME})/${SEND_INTERVAL}))
