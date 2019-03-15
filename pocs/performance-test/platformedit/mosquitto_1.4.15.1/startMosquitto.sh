e65a7ae4eb4163f499f5702a2df722a324c4e441
hiro-noguchi <noguchi0306@gmail.com>
2019-03-15 04:37:49 +0000

    fix performance-test

--- pocs/performance-test/platformedit/mosquitto_1.4.15.1/startMosquitto.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mosquitto_passwd -b /etc/mosquitto/pwfile ${IOT_USERNAME} ${IOTA_PASS}
-/usr/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
--- pocs/publish_container_startup_interval_number/del_containers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for container in `docker ps -a | grep dummy_device | awk '{print $1}'`;
-do
-    docker rm --force $container &
-    echo "Remove "$container
-done
